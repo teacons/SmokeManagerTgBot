87d68effea5ac44bf57a17e9171648ea0b0e0dbf
Igor Chernov <7767757+i-chernov@users.noreply.github.com>
2022-11-08 23:22:01 +0300

    Update Locale.kt

--- src/main/kotlin/ru/fbear/smokemanager/tg/Locale.kt
+++ src/main/kotlin/ru/fbear/smokemanager/tg/Locale.kt
@@ -1,10 +1,11 @@
 package ru.fbear.smokemanager.tg
 
-val START_DAY_MESSAGES = listOf("Time to work bitches!")
-val END_DAY_MESSAGES = listOf("Time to go home bitches!")
-val END_FRIDAY_MESSAGES = listOf("FRIDAY!! end of day. go to home")
-val SMOKE_TIME_START_MESSAGES = listOf("Time to smoke")
-val SMOKE_TIME_END_MESSAGES = listOf("Time to end smoke")
+val START_DAY_MESSAGES = listOf("Work time, bitchezzz!", "Да чо блин, опять?!", "Заткнись уже...это я себе", "May the force be with you", "Лучший день для суеты")
+val END_DAY_MESSAGES = listOf("Aliens, go home!", "Når kommer du hjem?", "Идите домой!", "Ну и чо сидим?")
+val END_FRIDAY_MESSAGES = listOf("FRIDAY!! Fucking week is over. Let's go dance", "Всмысле ты еще трезвый!", "Хороших выходных, увидимся в пн!")
+val SMOKE_TIME_START_MESSAGES = listOf("Time to smoke", "Го смок", "Реквестирую курение", "Пыхнем!", "Немножечко отдохнем", "Куряем", "Одна хорошо, а две лучше", "Поработал - покури!", "Курение вредит живым существам, ну а мы бессмертные пони. Поскакали")
+val SMOKE_TIME_END_MESSAGES = listOf("Back to work", "Пора работать", "Выдыхай", "Без труда не видать тебе зп", "Потом покуришь", "Курение пердит здоровью")
+val LUNCH_TIME = listOf("Что-то типа обэда", "Lunch time", "Bon Appétit")
 
 
 const val COMMAND_DESCRIPTION_SET_START_MONDAY = "Set monday start"
